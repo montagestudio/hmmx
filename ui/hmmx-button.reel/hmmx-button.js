46ecce379edeeb884d3b60cbd9203e990ad3768d
jonfu86 <jonfu86@gmail.com>
2017-03-31 14:11:35 -0700

    adding hmmx-button samples and reel based on abstract-button

---
+++ ui/hmmx-button.reel/hmmx-button.js
@@ -0,0 +1,9 @@
+var AbstractButton = require("montage/ui/base/abstract-button").AbstractButton;
+
+exports.HmmxButton = AbstractButton.specialize({
+
+    hasTemplate: {
+        value: false
+    }
+
+});
