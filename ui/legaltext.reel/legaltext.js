012b1d194450737db173c705182e8792d0070c00
jonfu86 <jonfu86@gmail.com>
2017-04-01 13:51:02 -0700

    adding formatting for main and adding legaltext example

---
+++ ui/legaltext.reel/legaltext.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/legaltext.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Legaltext
+ * @extends Component
+ */
+exports.Legaltext = Component.specialize(/** @lends Legaltext.prototype */{
+
+});
