c9af039d5fd871c03e3bc090e177e1619148525e
cdebost <corentin@montagestudio.com>
2017-04-01 13:48:03 -0700

    Add text field component

---
+++ ui/text-field.reel/text-field.js
@@ -0,0 +1,12 @@
+/**
+ * @module ui/text-field.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TextField
+ * @extends Component
+ */
+exports.TextField = Component.specialize(/** @lends TextField# */ {
+
+});
