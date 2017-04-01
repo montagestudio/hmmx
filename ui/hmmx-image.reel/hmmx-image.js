9497f4b9070f233f40239eb5fb9576bc2e7d061e
jonfu86 <jonfu86@gmail.com>
2017-03-31 19:09:04 -0700

    adding icon and images samples and importing image files

---
+++ ui/hmmx-image.reel/hmmx-image.js
@@ -0,0 +1,9 @@
+var AbstractImage = require("montage/ui/base/abstract-image").AbstractImage;
+
+exports.HmmxImage = AbstractImage.specialize({
+
+    hasTemplate: {
+        value: false
+    }
+
+});
