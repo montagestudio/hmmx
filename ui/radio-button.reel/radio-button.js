96f8c4b8f13aefcc60ca668fbacedeeb223ba5ef
cdebost <corentin@montagestudio.com>
2017-04-02 14:53:13 -0700

    Add radio button component

---
+++ ui/radio-button.reel/radio-button.js
@@ -0,0 +1,12 @@
+/**
+ * @module ui/radio-button.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class RadioButton
+ * @extends Component
+ */
+exports.RadioButton = Component.specialize(/** @lends RadioButton# */ {
+
+});
