841d89dfb7e120e0a1924da56c0d0a49927b1078
Benoit Marchant <marchant@mac.com>
2017-04-02 23:47:08 -0700

    Removes hmmx in component names

--- ui/hmmx-button.reel/hmmx-button.js
+++
@@ -1,9 +0,0 @@
-var AbstractButton = require("montage/ui/base/abstract-button").AbstractButton;
-
-exports.HmmxButton = AbstractButton.specialize({
-
-    hasTemplate: {
-        value: false
-    }
-
-});
--- ui/hmmx-image.reel/hmmx-image.js
+++
@@ -1,9 +0,0 @@
-var AbstractImage = require("montage/ui/base/abstract-image").AbstractImage;
-
-exports.HmmxImage = AbstractImage.specialize({
-
-    hasTemplate: {
-        value: false
-    }
-
-});
