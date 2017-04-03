a4086a82fdabfd85e95642ec8120eb2644384994
jonfu86 <jonfu86@gmail.com>
2017-04-02 22:29:47 -0700

    changed radio buttons in error and validation section to use radio-button-group

--- ui/form-group.reel/form-group.js
+++ ui/form-group.reel/form-group.js
@@ -1,8 +1,8 @@
 /**
  * @module "ui/form-group.reel"
  */
-var Component = require("montage/ui/component").Component,
-	PressComposer = require("montage/composer/press-composer").PressComposer;
+var Component = require("montage/ui/component").Component;
+	// PressComposer = require("montage/composer/press-composer").PressComposer;
 
 /**
  * @class FormGroup
--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -9,11 +9,5 @@ var Component = require("montage/ui/component").Component;
  */
 exports.Main = Component.specialize(/** @lends Main# */ {
 
-choices: {
-	value:[
-		{id:"yes", text: "Yes, my personal details are correct"},
-     	{id:"no", text: "No, some details are wrong or have changed"}
-	]	 
-}
 
 });
